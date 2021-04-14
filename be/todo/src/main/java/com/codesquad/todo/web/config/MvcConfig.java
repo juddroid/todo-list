207080eb771c7e24cf47dbc0bb745ae093bcf6b7
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-14 14:34:01 +0900

    feat : 불필요해보이는 CORS설정 제거

--- be/todo/src/main/java/com/codesquad/todo/web/config/MvcConfig.java
+++ be/todo/src/main/java/com/codesquad/todo/web/config/MvcConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class MvcConfig implements WebMvcConfigurer {
-    @Override
+    /*@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
                 .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("http://localhost:8080")
                 .allowedOrigins("http://ec2-13-209-60-60.ap-northeast-2.compute.amazonaws.com:3000")
                 .allowedOrigins("http://13.209.60.60:3000");
-    }
+    }*/
 }
