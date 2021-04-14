32abbf0a70689ececcc3107778581092f67a8993
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-14 10:48:35 +0900

    feat : 스프링 앱서버에 CORS 설정 추가

---
+++ be/todo/src/main/java/com/codesquad/todo/web/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.codesquad.todo.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://ec2-13-209-60-60.ap-northeast-2.compute.amazonaws.com:3000")
+                .allowedOrigins("http://13.209.60.60:3000");
+    }
+}
