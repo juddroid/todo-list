1e371c4ee9e197b91b27f8518f66f50ac630ea93
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-04-18 13:01:56 +0900

    feat : CORS 전역설정 추가

---
+++ be/todo/src/main/java/com/codesquad/todo/web/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.codesquad.todo.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("http://localhost:5000")
+        .allowedOrigins("http://13.209.60.60:5000")
+        .allowedOrigins("http://ec2-13-209-60-60.ap-northeast-2.compute.amazonaws.com:5000")
+        .allowedMethods("*");
+    }
+}
